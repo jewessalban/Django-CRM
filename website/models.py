0ed9db3885307effae6d768f82abb0a2bf60a0d4
Jewess Alban <jcalban3@up.edu.ph>
2025-01-03 20:53:32 +0800

    Final Project

--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	name = models.CharField(max_length=50)
+	quality =  models.CharField(max_length=50)
+	weapon =  models.CharField(max_length=100)
+	element = models.CharField(max_length=15)
+	region =  models.CharField(max_length=100)
+	
+
+	def __str__(self):
+		return(f"{self.name}")
